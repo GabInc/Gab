0f34a40e0cda3631eea7d135aaf319719e4d001a
akrfranko <franko@akr.club>
2014-12-11 14:19:32 -0500

    Grunt ghpages

--- app/scripts/concierge-icons-loader.js
+++ app/scripts/concierge-icons-loader.js
@@ -2,3 +2,6 @@
 grunticon( [ 'styles/concierge-icons-data-svg.css', 'styles/concierge-icons-data-png.css', 'styles/concierge-icons-fallback.css' ] );
 grunticon( [ 'styles/concierge-icons-data-svg.css', 'styles/concierge-icons-data-png.css', 'styles/concierge-icons-fallback.css' ] );
 grunticon( [ 'styles/concierge-icons-data-svg.css', 'styles/concierge-icons-data-png.css', 'styles/concierge-icons-fallback.css' ] );
+grunticon( [ 'styles/concierge-icons-data-svg.css', 'styles/concierge-icons-data-png.css', 'styles/concierge-icons-fallback.css' ] );
+grunticon( [ 'styles/concierge-icons-data-svg.css', 'styles/concierge-icons-data-png.css', 'styles/concierge-icons-fallback.css' ] );
+grunticon( [ 'styles/concierge-icons-data-svg.css', 'styles/concierge-icons-data-png.css', 'styles/concierge-icons-fallback.css' ] );
