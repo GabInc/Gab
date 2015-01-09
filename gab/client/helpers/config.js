feeef867e9726ad8b945775a425789e6465b78c0
Gabriel Dancause <gabrieldancause@gmail.com>
2015-01-08 22:00:12 -0500

    Changes to Facebook permission

--- gab/client/helpers/config.js
+++ gab/client/helpers/config.js
@@ -1,6 +1,6 @@
 Accounts.ui.config({
   requestPermissions: {
-    facebook: ['email', 'user_birthday', 'public_profile'],
+    facebook: ['email', 'user_birthday', 'public_profile', 'user_friends', 'user_likes', ],
   },
   requestOfflineToken: {
     google: true
